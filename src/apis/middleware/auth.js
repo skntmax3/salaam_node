dd36794cdb8c15cea063704b272a7f7d73efddba
Shashi Kant <shashi3.kant@paytm.com>
2025-02-12 22:37:12 +0530

    homepage done

--- src/apis/middleware/auth.js
+++ src/apis/middleware/auth.js
@@ -6,12 +6,13 @@ const jwt = require("../utils/jwt")
     try{
         const token = req.headers['authorization']
         const bearer_token = token.split(' ')[1]
-        
+
+
         if(!bearer_token) return res.send(` expired token or not a valid user`)  
 
         let decoded =await  jwt.verify(bearer_token )   
 
-        console.log("decoded", decoded)
+        console.log("decoded>>.", decoded)
         req.userObj ={}
         const { id} = decoded 
          req.token = `Bearer ${bearer_token}`  
